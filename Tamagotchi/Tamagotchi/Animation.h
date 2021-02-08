be67e0927351c10520d9ff3fd56f650a8df82daa
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-02-08 22:36:58 +0700

    Animation complete

--- Tamagotchi/Tamagotchi/Animation.h
+++ Tamagotchi/Tamagotchi/Animation.h
@@ -9,16 +9,18 @@ public:
 	Animation(Texture* texture, Vector2u imageCount, float switchTime);
 	~Animation();
 
+	void Update(int row, float deltaTime, bool faceRight);
+
 public:
 	IntRect uvRect;
-
+	float switchTime;
 
 private:
 	Vector2u imageCount;
 	Vector2u currentImage;
 
 	float totalTime;
-	float switchTime;
+	
 
 
 };
