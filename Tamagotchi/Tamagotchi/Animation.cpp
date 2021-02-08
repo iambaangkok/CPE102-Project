742f7eebdd6b459f43809b4ddec819a4cc262f22
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-02-08 19:31:50 +0700

    Initial commit

---
+++ Tamagotchi/Tamagotchi/Animation.cpp
@@ -0,0 +1,14 @@
+#include "Animation.h"
+
+
+
+Animation::Animation(Texture* texture, Vector2u imageCount, float switchTime) {
+
+	this->imageCount = imageCount;
+
+}
+
+
+Animation::~Animation() {
+
+}
