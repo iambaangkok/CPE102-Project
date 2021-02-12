b845ab8eee12627259797a741cc1f8f2dfbef8ca
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-02-12 20:02:06 +0700

    Added constructors and many variables

--- Tamagotchi/Tamagotchi/Pet.cpp
+++ Tamagotchi/Tamagotchi/Pet.cpp
@@ -4,9 +4,51 @@ Pet::Pet() : GameObject()
 {
 }
 
-Pet::Pet(Vector2f position, Vector2f dimensions, bool originIsCenter, string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime)
+Pet::Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//Proper Animation
+	string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime)
 	: GameObject(position, dimensions, originIsCenter, texturePath, imageCount, start, finish, frameTime)
 {
+}
+
+Pet::Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//Customize Pet
+	string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime,
+	string name, string type, int levelMax, vector<int> hpMax, vector<int> expPerEvolve, vector<int> happinessMax, vector<int> foodMax, vector<int> poopMax,
+	int hpChangeRate, int expChangeRate, int foodChangeRate, int happinessChangeRate, int poopChangeRate, float notEnoughFoodThreshold)
+	: GameObject(position, dimensions, originIsCenter, texturePath, imageCount, start, finish, frameTime)
+{
+	this->name = name;
+	this->type = type;
+	this->levelMax = levelMax;
+	currentLevel = 1;
+	this->hpMax = hpMax;
+	currentHp = this->hpMax[0];
+	this->expPerEvolve = expPerEvolve;
+	currentExp = expPerEvolve[0];
+	this->happinessMax = happinessMax;
+	currentHappiness = this->happinessMax[0];
+	this->foodMax = foodMax;
+	currentFood = this->foodMax[0];
+	this->poopMax = poopMax;
+	currentPoop = 0;
+	this->hpChangeRate = hpChangeRate;
+	this->expChangeRate = expChangeRate;
+	this->foodChangeRate = foodChangeRate;
+	this->happinessChangeRate = happinessChangeRate;
+	this->poopChangeRate = poopChangeRate;
+	this->notEnoughFoodThreshold = notEnoughFoodThreshold;
+}
+
+Pet::Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//By Type
+	string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime,
+	string name, string type) : GameObject(position, dimensions, originIsCenter, texturePath, imageCount, start, finish, frameTime)
+{
+	if (type == "Type1") {
+		//Set stuff
+	}
+	else if (type == "Type2") {
+		//Set more stuff
+	}
+
 }
 
 Pet::~Pet()
--- Tamagotchi/Tamagotchi/Pet.h
+++ Tamagotchi/Tamagotchi/Pet.h
@@ -1,21 +1,61 @@
 #pragma once
+#include <iostream>
+#include <chrono>
+#include <ctime>  
+#include <vector>
 #include "GameObject.h"
+
+using std::vector;
+
 class Pet :
     public GameObject
 {
 public:
 	Pet();
-	Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,
-		string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime); //Proper Animation
+	Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//Proper Animation
+		string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime);
+	Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//Customize Pet
+		string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime,
+		string name, string type, int levelMax, vector<int> hpMax, vector<int> expPerEvolve, vector<int> happinessMax, vector<int> foodMax, vector<int> poopMax,
+		int hpChangeRate = 1, int expChangeRate = 1, int foodChangeRate = 1, int happinessChangeRate = 1, int poopChangeRate = 1, float notEnoughFoodThreshold = 20);
+	Pet(Vector2f position, Vector2f dimensions, bool originIsCenter,//By Type
+		string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime, 
+		string name, string type);
 	~Pet();
 
+
 	string name;
-	int levelMax, currentLevel;
-	int hpMax, currentHp;
-	int expMax, currentExp;
-	int happinessMax, currentHappiness;
-	int foodMax, currentFood;
-	int poopMax, currentPoop;
+	string type;
+	int currentLevel;
+	int currentHp;
+	int currentExp;
+	int currentHappiness;
+	int currentFood;
+	int currentPoop;
+
+	float hpChangeRateMultiplier = 1.0f;
+	float expChangeMultiplier = 1.0f;
+	float foodChangeMultiplier = 1.0f;
+	float happinessChangeMultiplier = 1.0f;
+	float poopChangeMultiplier = 1.0f;
+
+
+private:
+	int levelMax;
+	vector<int> hpMax; //HP
+	vector<int> expPerEvolve; //EXP
+	vector<int> happinessMax; //Happiness
+	vector<int> foodMax; //Food
+	vector<int> poopMax; //Poop
+
+	//All Rate are at 1 per X minute;
+	int hpChangeRate = 1;
+	int expChangeRate = 1;
+	int foodChangeRate = 1;
+	int happinessChangeRate = 1;
+	int poopChangeRate = 1;
+
+	float notEnoughFoodThreshold = 20; //If Food is below this PERCENT, HP starts to decay
 
 };
 
