b1a4991c23c3514c49443a4873428c9905ab39e8
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-03-17 17:48:35 +0700

    Fixed Merging Issue

--- Tamagotchi/Tamagotchi/Button.h
+++ Tamagotchi/Tamagotchi/Button.h
@@ -14,19 +14,19 @@ public:
     Button();
     Button(Vector2f position, Vector2f dimensions, bool originIsCenter,
         string texturePath, Vector2u imageCount, Vector2i start, Vector2i finish, float frameTime
-        , string text, int status, string type, int& gstate, Shop& shop, Pet& pet,Doodle &doodle,Game &game, int id = -69); //Proper Animation
+        , string text, int status, string type, int& gstate, Shop& shop, Pet& pet,Doodle &doodle, int id = -69); //Proper Animation
     ~Button();
 
     void Initialize();//Runs before everything else in every game loop/ reset variable that needs to be reset every game loop
 
-    void Update(float deltaTime,RenderWindow& window,unordered_map<string, bool>& mousePress, unordered_map<string, bool>& mouseHold, Vector2i& mousePosition, bool& quitGame, int& selectedPet);
+    void Update(float deltaTime,RenderWindow& window,unordered_map<string, bool>& mousePress, unordered_map<string, bool>& mouseHold, Vector2i& mousePosition, bool& quitGame, int& selectedPet, bool& clearSave, bool& muteBgm, bool& muteSfx);
 
     
 
-    void OnClick();
+    void OnClick(bool& muteBgm, bool& muteSfx);
     void OnHover();
     void OnHold();
-    void OnRelease(bool& quitGame, int& selectedPet);
+    void OnRelease(bool& quitGame, int& selectedPet, bool& clearSave);
     bool IsMouseOver(Vector2i& mousePosition);
 
 
@@ -38,7 +38,6 @@ public:
     Shop* shop;
     Pet* pet;
     Doodle* doodle;
-    Game* game;
     bool isPressed = false;
     int id;
 };
--- Tamagotchi/Tamagotchi/Shop.cpp
+++ Tamagotchi/Tamagotchi/Shop.cpp
@@ -2,18 +2,17 @@
 #include "Item.h"
 Shop::Shop() {
   
-    Font font;
-    if (!font.loadFromFile("Assets/Fonts/arial.ttf"))
+    if (!font.loadFromFile("Assets/Fonts/Minecraftia.ttf"))
         throw(" CLOUD NOT LOAD FONT! ");
-    //for (int i = 0; i < items.size(); ++i) {
-        //Text description;
-        //description.setFont(font);
-        //description.setFillColor(Color(219, 138, 6, 125));
-        //description.setCharacterSize(25);
-        //description.setString(items[i]->description);
-        //description.setPosition(100, 1040 / 2);
-        //descriptions.push_back(description);
-    //}
+    for (int i = 0; i < items.size(); ++i) {
+        Text description;
+        description.setFont(font);
+        description.setFillColor(Color(219, 138, 6, 125));
+        description.setCharacterSize(25);
+        description.setString(items[i]->description);
+        description.setPosition(100, 1040 / 2);
+        descriptions.push_back(description);
+    }
     AddItem("hotdog","ShopItem8.png");
     AddItem("noodle69","ShopItem9.png");
     AddItem("chicken drumstick","ShopItem14.png");
@@ -26,26 +25,26 @@ Shop::Shop() {
     AddItem("mochi","ShopItem10.png");
     AddItem("dango","ShopItem11.png");
    
+
     AddItem("evo1","ShopItem1.png");
     AddItem("evo2","ShopItem1.png");
     AddItem("evo3","ShopItem1.png");
 
-
-
-
+  
     
     for (int i = 0; i < itemfood.size(); i++)    
     {
-        itemfood[i]->SetPosition(corePosition, (i * 180) + 270);
+        itemfood[i]->SetPosition(corePosition, (i * 180) + 470);
     }//food
     for (int i = 0; i < itemcandy.size(); i++)
     {
-        itemcandy[i]->SetPosition(corePosition, (i * 180) + 270);
+        itemcandy[i]->SetPosition(corePosition, (i * 180) + 470);
     }//candy
     for (int i = 0; i < itemetc.size(); i++)
     {
-        itemetc[i]->SetPosition(corePosition, (i * 180) + 270);
+        itemetc[i]->SetPosition(corePosition, (i * 180) + 470);
     }//etc
+    
     //static GameObject bgr = GameObject(Vector2f(0, 0), Vector2f(windowWidth, windowHeight), false,"Assets/Textures/button_manu1.png", Vector2u(1, 1), Vector2i(0, 0), Vector2i(0, 0), 1);
     // bg = &bgr; 
 
@@ -56,8 +55,6 @@ Shop::Shop() {
     static GameObject scroll = GameObject(Vector2f(positionscrollX , 200), Vector2f(10, heightscrollbar), false, "Assets/Textures/panel_blue_72x20.png", Vector2u(1, 1), Vector2i(0, 0), Vector2i(0, 0), 1);
     scrollbar = &scroll; //Scroll
     
-
-
 
    //static GameObject item = GameObject(Vector2f(corePosition, 270), Vector2f(400, 180), false, "Assets/Textures/Shop/item/ShopItem99.png", Vector2u(1, 1), Vector2i(0, 0), Vector2i(0, 0), 1);
    // picitem = &item; // shop in item.
@@ -79,49 +76,97 @@ void Shop::AddItem(string itemName, string texturePath) {
     Vector2i start(0, 0);
     Vector2i finish(0, 0);
 
+    int fontSize = 14;
     //static GameObject item = GameObject(Vector2f(corePosition, 270), Vector2f(400, 180), false, "Assets/Textures/Shop/item/ShopItem99.png", Vector2u(1, 1), Vector2i(0, 0), Vector2i(0, 0), 1);
     Item* newItem = new Item(Vector2f(0, 0), itemPictureSize, false, "Assets/Textures/Shop/" + texturePath, imageCount, start, finish, 1, itemName);
     if (newItem->type == "food")
     {
         itemfood.push_back(newItem);
+
+        vector<Text> thisItemsText;
+
+        for(int i = 0  ; i < 5 ;++i)
+            thisItemsText.push_back(Text());
+        for (int i = 0; i < itemfood.size();++i) {
+            SetTextShop(thisItemsText[0], newItem->name, font, col_BLACK1, 20, Vector2f(corePosition + 80, (i * 180) + 470));
+            // SetTextShop(thisItemsText[1], to_string(newItem->hpChange), font, col_BLACK1, fontSize, Vector2f(corePosition+50, corePositiony));
+            // SetTextShop(thisItemsText[2], to_string(newItem->foodChange), font, col_BLACK1, fontSize, Vector2f(corePosition+55, corePositiony));
+            // SetTextShop(thisItemsText[3], to_string(newItem->happinessChange), font, col_BLACK1, fontSize, Vector2f(corePosition+60, corePositiony));
+            // SetTextShop(thisItemsText[4], to_string(newItem->price), font, col_BLACK1, fontSize, Vector2f(corePosition, corePositiony));
+        }
+        food.push_back(thisItemsText);
     }
     if (newItem->type == "candy")
     {
         itemcandy.push_back(newItem);
+        vector<Text> thisItemsText;
+
+       /* for (int i = 0; i < 5; ++i)
+            thisItemsText.push_back(Text());
+
+        SetTextShop(thisItemsText[0], newItem->name, font, col_BLACK1, 20, Vector2f(corePosition+80, corePositiony));
+        SetTextShop(thisItemsText[1], to_string(newItem->hpChange), font, col_BLACK1, fontSize, Vector2f(corePosition+50, corePositiony));
+        SetTextShop(thisItemsText[2], to_string(newItem->foodChange), font, col_BLACK1, fontSize, Vector2f(corePosition+55, corePositiony));
+        SetTextShop(thisItemsText[3], to_string(newItem->happinessChange), font, col_BLACK1, fontSize, Vector2f(corePosition+60, corePositiony));
+        SetTextShop(thisItemsText[4], to_string(newItem->price), font, col_BLACK1, fontSize, Vector2f(corePosition, corePositiony));
+
+        candy.push_back(thisItemsText);*/
     }
     if (newItem->type == "etc")
     {
         itemetc.push_back(newItem);
-    }
+       /* vector<Text> thisItemsText;
 
+        for (int i = 0; i < 5; ++i)
+            thisItemsText.push_back(Text());
+
+        SetTextShop(thisItemsText[0], newItem->name, font, col_BLACK1, 20, Vector2f(corePosition+80, corePositiony));
+        SetTextShop(thisItemsText[1], to_string(newItem->hpChange), font, col_BLACK1, fontSize, Vector2f(corePosition+50, corePositiony));
+        SetTextShop(thisItemsText[2], to_string(newItem->foodChange), font, col_BLACK1, fontSize, Vector2f(corePosition+55, corePositiony));
+        SetTextShop(thisItemsText[3], to_string(newItem->happinessChange), font, col_BLACK1, fontSize, Vector2f(corePosition+60, corePositiony));
+        SetTextShop(thisItemsText[4], to_string(newItem->price), font, col_BLACK1, fontSize, Vector2f(corePosition, corePositiony));
+
+        etc.push_back(thisItemsText);*/
+    }
 }
 void Shop::Draw(RenderWindow &window) {
     if (isOpen) {
         //bg->Draw(window);
-        up->Draw(window);
-        bgbar->Draw(window);
         scrollbar->Draw(window);
         //picitem->Draw(window);
-        if (isOpen) {
+
+        if (status == "food") {
             for (int i = 0; i < itemfood.size(); i++)
             {
                 itemfood[i]->Draw(window);
+                for (int j = 0; j < food[i].size(); ++j) {
+                    window.draw(food[i][j]);
+                }
             }
+           
         }
-        if (0){
+        if (status == "candy"){
             for (int i = 0; i < itemcandy.size(); i++)
             {
                 itemcandy[i]->Draw(window);
+                for (int j = 0; j < candy[i].size(); ++j) {
+                    window.draw(candy[i][j]);
+                }
             }
         }
-        if (0) {
+        if (status == "etc") {
             for (int i = 0; i < itemetc.size(); i++)
             {
                 itemetc[i]->Draw(window);
+                for (int j = 0; j < etc[i].size(); ++j) {
+                    window.draw(etc[i][j]);
+                }
             }
         }
+        
+        bgbar->Draw(window);
+        up->Draw(window);
         low->Draw(window);
-        //test1->Draw(window);
     }
     else {
     }
@@ -129,7 +174,7 @@ void Shop::Draw(RenderWindow &window) {
 
 void Shop::Update(float deltaTime, int mouseWheelDelta) {
     if (isOpen) {
-        //cout << mouseWheelDelta;
+        cout << mouseWheelDelta;
         if (mouseWheelDelta != 0){
             scrollbar->SetPosition(positionscrollX,scrollbar->GetPosition().y+0-mouseWheelDelta*deltaTime*speedscroll);
         }
@@ -139,54 +184,39 @@ void Shop::Update(float deltaTime, int mouseWheelDelta) {
         if (scrollbar->GetPosition().y+heightscrollbar > lowscroll) {
             scrollbar->SetPosition(Vector2f(positionscrollX, lowscroll-heightscrollbar));
         }
+
         float timey = scrollbar->GetPosition().y + 0 - mouseWheelDelta * deltaTime * speedscroll;
-       /* for (int i = 1; i < itemfood.size(); i++)
-        {
-            itemfood[i]->SetPosition(corePosition, (i * 180) + 270);
-        }//food
-        for (int i = 1; i < itemcandy.size(); i++)
-        {
-            itemcandy[i]->SetPosition(corePosition, (i * 180) + 270);
-        }//candy
-        for (int i = 1; i < itemetc.size(); i++)
-        {
-            itemetc[i]->SetPosition(corePosition, (i * 180) + 270);
-        }*/
-        
-        if (isOpen) {
+       
+        if (status=="food") {
+           
             for (int i = 0; i < itemfood.size(); i++)
             {
-                itemfood[i]->Update(deltaTime);
-            }
-            for (int i = 1; i < itemfood.size(); i++)
-            {
-                itemfood[i]->SetPosition(corePosition, (i * 180) + corePositiony - timey);
+                itemfood[i]->SetPosition(corePosition, ((i * 180) + corePositiony+200) - timey);
+                 
             }//food
-
-
-        } // �Ѿവ 
-        if (0) {
+        } // 
+        if (status=="candy") {
+           
             for (int i = 0; i < itemcandy.size(); i++)
             {
-                itemcandy[i]->Update(deltaTime);
-            }
-            for (int i = 1; i < itemcandy.size(); i++)
-            {
-                itemcandy[i]->SetPosition(corePosition, (i * 180) + corePositiony - timey);
+                itemcandy[i]->SetPosition(corePosition, ((i * 180) + corePositiony+200) - timey);
             }//candy
         }
-        if (0) {
+        if (status=="etc") {
+           
             for (int i = 0; i < itemetc.size(); i++)
             {
-                itemetc[i]->Update(deltaTime);
-            }
-            for (int i = 1; i < itemetc.size(); i++)
-            {
-                itemetc[i]->SetPosition(corePosition, (i * 180) + corePositiony - timey);
+                itemetc[i]->SetPosition(corePosition, ((i * 180) + corePositiony+200)- timey);
             }
         }
         //test1->Update(deltaTime);
     }
 }
 
-
+void Shop::SetTextShop(Text& text, string str, Font& font, Color color, int size, Vector2f position) {
+    text.setString(str);
+    text.setFont(font);
+    text.setFillColor(color);
+    text.setCharacterSize(size);
+    text.setPosition(position);
+}
